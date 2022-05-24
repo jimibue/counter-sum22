928dc1a7abf5d9967339636a1bfd4b2b9543e870
James <jimibue@gmail.com>
2022-05-24 11:45:21 -0600

    counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Counter from "./Counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Counter Example</h1>
+      <div className="counters">
+       <Counter name='Steps' goal={10000} />    
+       <Counter name='ScreenTime' goal={8} />    
+      </div>
     </div>
   );
 }
